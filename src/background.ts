ead307e8cf867867d8eee5d95d787fd788c369ea
Vicente Alvaro <vicenalvaro@gmail.com>
2023-12-10 18:19:18 +0100

    Mejoras

--- src/background.ts
+++ src/background.ts
@@ -22,60 +22,60 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.contextMenus.onClicked.addListener((event) => {
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
     if (tabs.length > 0 && tabs[0].id) {
-      chrome.tabs.sendMessage(tabs[0].id, { type: "loading" });
+      const tabId = tabs[0].id;
+      sendTabMessage(tabId, "loading");
       try {
         const prompt = await prompts.getPrompt(event);
         const response = await AI.requestInfo(prompt);
-
-        const resRead = (response as ReadableStream<Uint8Array>).getReader();
-        while (true) {
-          const { done, value } = await resRead.read();
-          if (done) break;
-          if (done === undefined || value === undefined) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-              type: "error",
-              data: { error: "ERROR" },
-            });
-          }
-          const answer = new TextDecoder().decode(value);
-          try {
-            const res = await answer.split("data:");
-            try {
-              const detail = JSON.parse(res[0]).detail;
-              chrome.tabs.sendMessage(tabs[0].id, {
-                type: "text",
-                data: { text: detail },
-              });
-            } catch (e) {
-              try {
-                const resTrim = res[1].trim();
-                if (resTrim === "[DONE]") return;
-                const answerJson = JSON.parse(resTrim);
-                let final = answerJson.message.content.parts[0];
-                final = final.replace(/\n/g, "<br>");
-                chrome.tabs.sendMessage(tabs[0].id, {
-                  type: "text",
-                  data: { text: final },
-                });
-              } catch (e) {
-                // no se hacer nada
-              }
-            }
-          } catch (e) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-              type: "error",
-              data: {
-                error: `Something went wrong. Please try in a few minutes. ${e}`,
-              },
-            });
-          }
-        }
+        processStreamResponse(response as ReadableStream<Uint8Array>, tabId);
       } catch (e) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          type: "error",
-          data: { error: e },
-        });
+        sendTabMessageError(tabId, { error: e });
       }
     }
   });
 });
+
+async function processStreamResponse(
+  response: ReadableStream<Uint8Array>,
+  tabId: number
+) {
+  const resRead = response.getReader();
+  while (true) {
+    const { done, value } = await resRead.read();
+    if (done) break;
+    if (done === undefined || value === undefined) {
+      sendTabMessageError(tabId, { error: "ERROR" });
+    }
+    const answer = new TextDecoder().decode(value);
+    try {
+      const res = await answer.split("data:");
+      try {
+        const detail = JSON.parse(res[0]).detail;
+        sendTabMessage(tabId, "text", { text: detail });
+      } catch (e) {
+        try {
+          const resTrim = res[1].trim();
+          if (resTrim === "[DONE]") return;
+          const answerJson = JSON.parse(resTrim);
+          let final = answerJson.message.content.parts[0];
+          final = final.replace(/\n/g, "<br>");
+          sendTabMessage(tabId, "text", { text: final });
+        } catch (e) {
+          // no se hacer nada
+        }
+      }
+    } catch (e) {
+      sendTabMessageError(tabId, {
+        error: `Something went wrong. Please try in a few minutes. ${e}`,
+      });
+    }
+  }
+}
+
+function sendTabMessage(tabId: number, type: string, data: any = {}) {
+  chrome.tabs.sendMessage(tabId, { type, data });
+}
+
+function sendTabMessageError(tabId: number, data: any) {
+  sendTabMessage(tabId, "error", data);
+}
