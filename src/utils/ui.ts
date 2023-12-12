cc3864c53741c1628d7cfdb4e42973428f00acd7
Vicente Alvaro <vicenalvaro@gmail.com>
2023-12-12 22:21:34 +0100

    Cierre de ventana

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,9 +1,11 @@
 export class UI {
   private element: HTMLElement;
+  private elementContent: HTMLElement;
   private prefixCSS: string;
 
   constructor(prefixCSS = "mt") {
     this.element = document.createElement("div");
+    this.elementContent = document.createElement("div");
     this.prefixCSS = prefixCSS;
   }
 
@@ -12,45 +14,46 @@ export class UI {
     this.element.classList.add(this.prefixCSS);
     this.element.classList.add(`${this.prefixCSS}-modal`);
 
-    this.element.innerHTML = `
-      <div class="${this.prefixCSS}-modal-content">
-        <span class="${this.prefixCSS}-modal-content-close">×</span>
-        <div class="${this.prefixCSS}-modal-content-header">
-          <img class="${
-            this.prefixCSS
-          }-modal-content-header-logo" src="${chrome.runtime.getURL(
+    this.elementContent.classList.add(`${this.prefixCSS}-modal-content`);
+
+    this.elementContent.innerHTML = `
+      <span class="${this.prefixCSS}-modal-content-close">×</span>
+      <div class="${this.prefixCSS}-modal-content-header">
+        <img class="${
+          this.prefixCSS
+        }-modal-content-header-logo" src="${chrome.runtime.getURL(
       "assets/icons/icon.png"
     )}" alt="Mucho texto">
-          <div class="${this.prefixCSS}-modal-content-header-wrapper-title">
-            <h2 class="${this.prefixCSS}-modal-content-header-subtitle"></h2>
-            <h1 class="${this.prefixCSS}-modal-content-header-title"></h1>
-          </div>
+        <div class="${this.prefixCSS}-modal-content-header-wrapper-title">
+          <h2 class="${this.prefixCSS}-modal-content-header-subtitle"></h2>
+          <h1 class="${this.prefixCSS}-modal-content-header-title"></h1>
         </div>
-        <div class="${this.prefixCSS}-modal-content-main">
-          <div class="${this.prefixCSS}-modal-content-loading">
-            <div class="${this.prefixCSS}-modal-content-loading-loader">
-              <div class="mt-loader"></div>
-            </div>
-            <p class="${this.prefixCSS}-modal-content-loading-text"></p>
+      </div>
+      <div class="${this.prefixCSS}-modal-content-main">
+        <div class="${this.prefixCSS}-modal-content-loading">
+          <div class="${this.prefixCSS}-modal-content-loading-loader">
+            <div class="mt-loader"></div>
           </div>
-          <div class="${this.prefixCSS}-modal-content-result">
-            <p class="${this.prefixCSS}-modal-content-result-text"></p>
-          </div>
+          <p class="${this.prefixCSS}-modal-content-loading-text"></p>
         </div>
-        <div class="${this.prefixCSS}-modal-content-actions">
-          <div class="${this.prefixCSS}-modal-content-actions-wrapper">
-            <div class="${this.prefixCSS}-modal-content-actions-list"></div>
-            <div>
-              <a href="https://www.buymeacoffee.com/vicenalvaro" target="_blank">
-                <img class="mt-buymecoffee" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
-              </a>
-            </div>
+        <div class="${this.prefixCSS}-modal-content-result">
+          <p class="${this.prefixCSS}-modal-content-result-text"></p>
+        </div>
+      </div>
+      <div class="${this.prefixCSS}-modal-content-actions">
+        <div class="${this.prefixCSS}-modal-content-actions-wrapper">
+          <div class="${this.prefixCSS}-modal-content-actions-list"></div>
+          <div>
+            <a href="https://www.buymeacoffee.com/vicenalvaro" target="_blank">
+              <img class="mt-buymecoffee" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
+            </a>
           </div>
         </div>
       </div>`;
 
     this.createListeners();
 
+    this.element.appendChild(this.elementContent);
     document.body.appendChild(this.element);
 
     return this;
@@ -119,6 +122,10 @@ export class UI {
 
   private handleCloseClick = () => {
     this.closeModal();
+  };
+
+  private handleCloseClickStop = (event: MouseEvent) => {
+    if (event) event.stopPropagation();
   };
 
   private openModal() {
@@ -199,7 +206,8 @@ export class UI {
       this.handleCloseClick
     );
 
-    // window.addEventListener("click", this.handleCloseClick);
+    this.elementContent.addEventListener("click", this.handleCloseClickStop);
+    window.addEventListener("click", this.handleCloseClick);
   }
 
   private destroyListeners() {
@@ -207,7 +215,9 @@ export class UI {
       "click",
       this.handleCloseClick
     );
-    // window.removeEventListener("click", this.handleCloseClick);
+
+    this.elementContent.removeEventListener("click", this.handleCloseClickStop);
+    window.removeEventListener("click", this.handleCloseClick);
   }
 
   public destroy() {
@@ -215,56 +225,62 @@ export class UI {
   }
 
   private getElementContentCloseBtn() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-close`
     )[0];
   }
 
   private getElementContentLoading() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-loading`
     )[0];
   }
 
   private getElementContentLoadingText() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-loading-text`
     )[0];
   }
 
   private getElementContentTitle() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-header-title`
     )[0];
   }
 
   private getElementContentSubTitle() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-header-subtitle`
     )[0];
   }
 
   private getElementContentResult() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-result`
     )[0];
   }
 
   private getElementContentResultText() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-result-text`
     )[0];
   }
 
   private getElementContentActions() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-actions`
     )[0];
   }
 
   private getElementContentActionsList() {
-    return this.element.getElementsByClassName(
+    return this.elementContent.getElementsByClassName(
       `${this.prefixCSS}-modal-content-actions-list`
+    )[0];
+  }
+
+  private getElementContent() {
+    return this.elementContent.getElementsByClassName(
+      `${this.prefixCSS}-modal-content`
     )[0];
   }
 
