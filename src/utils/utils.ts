6c9c46a64586e02920f646f1d9c51fabcef51050
Vicente Alvaro <vicenalvaro@gmail.com>
2023-12-21 11:05:13 +0100

    Cambio de server

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,21 +2,20 @@ function sleep(ms: number) {
   return new Promise((r) => setTimeout(r, ms));
 }
 
+const url = "https://vicenalvaro.alwaysdata.net/";
+
 async function getArticle(link: string, controller: AbortController) {
   try {
-    const response = await fetch(
-      "https://article-extractor-api.onrender.com/",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          link,
-        }),
-        signal: controller.signal,
-      }
-    );
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        link,
+      }),
+      signal: controller.signal,
+    });
 
     if (response.ok) {
       const result = await response.json();
